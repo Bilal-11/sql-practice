7e725f12ca4d84f19453ab29fc4c21e2cddc360b
Sheikh Bilal Ahmad <sbilalahmad2000@gmail.com>
2026-05-16 23:34:49 +0100

    Unfinished solution for Leetcode 1321

---
+++ week3/2026-05-16/q20_restaurant_growth.sql
@@ -0,0 +1,95 @@
+/*
+Problem:
+Table: Customer
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| customer_id   | int     |
+| name          | varchar |
+| visited_on    | date    |
+| amount        | int     |
++---------------+---------+
+In SQL,(customer_id, visited_on) is the primary key for this table.
+This table contains data about customer transactions in a restaurant.
+visited_on is the date on which the customer with ID (customer_id) has visited the restaurant.
+amount is the total paid by a customer.
+ 
+
+You are the restaurant owner and you want to analyze a possible expansion (there will be at least one customer every day).
+
+Compute the moving average of how much the customer paid in a seven days window (i.e., current day + 6 days before). average_amount should be rounded to two decimal places.
+
+Return the result table ordered by visited_on in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customer table:
++-------------+--------------+--------------+-------------+
+| customer_id | name         | visited_on   | amount      |
++-------------+--------------+--------------+-------------+
+| 1           | Jhon         | 2019-01-01   | 100         |
+| 2           | Daniel       | 2019-01-02   | 110         |
+| 3           | Jade         | 2019-01-03   | 120         |
+| 4           | Khaled       | 2019-01-04   | 130         |
+| 5           | Winston      | 2019-01-05   | 110         | 
+| 6           | Elvis        | 2019-01-06   | 140         | 
+| 7           | Anna         | 2019-01-07   | 150         |
+| 8           | Maria        | 2019-01-08   | 80          |
+| 9           | Jaze         | 2019-01-09   | 110         | 
+| 1           | Jhon         | 2019-01-10   | 130         | 
+| 3           | Jade         | 2019-01-10   | 150         | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+--------------+----------------+
+| visited_on   | amount       | average_amount |
++--------------+--------------+----------------+
+| 2019-01-07   | 860          | 122.86         |
+| 2019-01-08   | 840          | 120            |
+| 2019-01-09   | 840          | 120            |
+| 2019-01-10   | 1000         | 142.86         |
++--------------+--------------+----------------+
+Explanation: 
+1st moving average from 2019-01-01 to 2019-01-07 has an average_amount of (100 + 110 + 120 + 130 + 110 + 140 + 150)/7 = 122.86
+2nd moving average from 2019-01-02 to 2019-01-08 has an average_amount of (110 + 120 + 130 + 110 + 140 + 150 + 80)/7 = 120
+3rd moving average from 2019-01-03 to 2019-01-09 has an average_amount of (120 + 130 + 110 + 140 + 150 + 80 + 110)/7 = 120
+4th moving average from 2019-01-04 to 2019-01-10 has an average_amount of (130 + 110 + 140 + 150 + 80 + 110 + 130 + 150)/7 = 142.86
+ 
+
+*/
+/*
+Source: Leetcode  1321 - https://leetcode.com/problems/restaurant-growth/description/
+*/
+/* Difficulty: Medium */
+/* Topic: Window function - running total using SUM */
+/*
+Approach:
+1. Firstly, we notice that one visited_on date can appear multiple times, thus we aggregate the amount for that date using SUM with GROUP BY.
+2. Since GROUP BY will be executed before SELECT, we can use AVG as a window function in the SELECT clause.
+3. In the window function usage, we need to fix the window size to be past 7 days (including the current day). So, we use ROWS BETWEEN to do so. We also ORDER the series BY visited_on date. The result is the moving average with a window size of 1 week.
+4. Lastly, offset the result set by 6 to start the result at the 7th day.
+*/
+
+SELECT visited_on, SUM(amount) AS amount, AVG(amount) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ORDER BY visited_on) AS average_amount
+FROM Customer
+GROUP BY visited_on
+ORDER BY visited_on
+OFFSET 6;
+
+/*
+You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'ORDER BY visited_on) AS average_amount
+FROM Customer
+GROUP BY visited_on
+ORDER B' at line 2
+*/
+
+--UNFINISHED SOLUTION
+
+/*
+What I learned / mistakes made: 
+*/
